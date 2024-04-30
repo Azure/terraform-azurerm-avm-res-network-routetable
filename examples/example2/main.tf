1eee279037513675975ebd52fbfb880cdbe067d3
Adam <adammontlake@microsoft.com>
2024-04-30 15:34:38 +0300

    fmt

--- examples/example2/main.tf
+++ examples/example2/main.tf
@@ -45,18 +45,18 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_virtual_network" "this" {
-  location                = azurerm_resource_group.this.location
-  name                    = module.naming.virtual_network.name
-  resource_group_name     = azurerm_resource_group.this.name
-  address_space           = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.virtual_network.name
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "this" {
-  count                   = 2
-  name                    = module.naming.subnet.name[count.index]
-  resource_group_name     = azurerm_resource_group.this.name
-  virtual_network_name    = azurerm_virtual_network.this.name
-  address_prefixes        = ["10.0.${count.index}.0/24"]
+  count                = 2
+  name                 = module.naming.subnet.name[count.index]
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.${count.index}.0/24"]
 }
 
 # This is the module call
