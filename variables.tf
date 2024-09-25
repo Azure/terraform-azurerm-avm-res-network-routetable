6601a4f06bf9a653b36ac39d6da82cab1d18d650
adammontlake <92934931+adammontlake@users.noreply.github.com>
2024-09-25 11:37:59 +0300

    Add support for AzureRM v4 (#33)
    * Add support for AzureRM v4
    
    * add ignore to plan files in test folder
    
    * Update docs

--- variables.tf
+++ variables.tf
@@ -19,10 +19,10 @@ variable "resource_group_name" {
   nullable    = false
 }
 
-variable "disable_bgp_route_propagation" {
+variable "bgp_route_propagation_enabled" {
   type        = bool
   default     = true
-  description = "(Optional) Boolean flag which controls propagation of routes learned by BGP on that route table. True means disable."
+  description = "(Optional) Boolean flag which controls propagation of routes learned by BGP on that route table. Defaults to true."
 }
 
 variable "enable_telemetry" {
@@ -133,7 +133,7 @@ variable "subnet_resource_ids" {
 ```yaml annotate 
 /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/virtualNetworks/{vnetName}/subnets/{subnetName}
 ```
-
+Example Input:
 ```terraform
 subnet_resource_ids = {
     subnet1 = azurerm_subnet.this.id,
