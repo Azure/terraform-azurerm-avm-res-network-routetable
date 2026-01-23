033b6f44a415145c1c8037ec21e96873bd756826
Jared Holgate <jaredholgate@microsoft.com>
2026-01-23 10:38:02 +0000

    fix module call

--- main.tf
+++ main.tf
@@ -15,8 +15,7 @@ module "routes" {
   address_prefix         = each.value.address_prefix
   name                   = each.value.name
   next_hop_type          = each.value.next_hop_type
-  resource_group_name    = var.resource_group_name
-  route_table_name       = azurerm_route_table.this.name
+  parent_id              = azurerm_route_table.this.id
   next_hop_in_ip_address = each.value.next_hop_in_ip_address
 }
 
